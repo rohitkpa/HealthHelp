2afb6911426e5009a50d933f27b79b0555c795ff
boice wong <boicewong1@gmail.com>
2018-01-13 05:13:01 -0700

    added burns and stab file

---
+++ src/burns_dt.py
@@ -0,0 +1,2 @@
+def stabbing_dt_ask():
+    return "1. Stop burning. /n2. Remove constrictive clothing. /n3. For first-degree burns, cool burn, protect burn wuth sterile gauze or cloth, and then treat burn."
--- src/pregnancy_dt.py
+++ src/pregnancy_dt.py
@@ -1,2 +1,2 @@
-x = "Get the individual on their back. Tell the individual to breathe. If contractions are less than two minutes, birth is imminent. Ensure that as the baby comes out, you do not pull on the baby and that you do not drop the baby."
-print(x)
+def pregnancy_dt_ask():
+    return "1. Get the individual on their back. /n2. Tell the individual to breathe. /n3. If contractions are less than two minutes, birth is imminent. /n4. Ensure that as the baby comes out, you do not pull on the baby and that you do not drop the baby."
