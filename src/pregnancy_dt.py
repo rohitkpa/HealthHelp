9ac6937b095aec484cd136ebf08a408a1300ac6b
boice wong <boicewong1@gmail.com>
2018-01-13 05:00:38 -0700

    added preg file

---
+++ src/pregnancy_dt.py
@@ -0,0 +1,2 @@
+x = "Get the individual on their back. Tell the individual to breathe. If contractions are less than two minutes, birth is imminent. Ensure that as the baby comes out, you do not pull on the baby and that you do not drop the baby."
+print(x)
