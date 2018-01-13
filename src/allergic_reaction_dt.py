d5cdd47655ba7641d5efa1bccdfe6aae1ca87a9f
Rohit Kumar <rohitkpa@gmail.com>
2018-01-13 05:29:38 -0700

    added files

---
+++ src/allergic_reaction_dt.py
@@ -0,0 +1,2 @@
+def allergic_reaction_dt():
+	return "1. Apply allergy medication if applicable\n2. If the individual is unconscious, apply CPR"
---
+++ src/breathing_difficulty_dt.py
@@ -0,0 +1,2 @@
+def breathing_difficulty_dt():
+	return "1. Begin CPR\n2. Loosen any tight clothing\n3. Help the person use any prescribed medicine such as home oxygen or an inhaler\n4. Close any open wounds"
