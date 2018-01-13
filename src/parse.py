36116d5234a663580cf33bb725b2469231946953
Rohit Kumar <rohitkpa@gmail.com>
2018-01-13 14:34:41 -0700

    changed stabbing response

--- src/parse.py
+++ src/parse.py
@@ -54,13 +54,13 @@ def hyperthermia_dt():
 def hypohyperglycemia_dt():
     return "\n\n1. If unconscious, roll the individual on their side and attempt to call for medical attention. \n2. If conscious, attempt to give the patient some sugar if hypoglycemic. \n3. If conscious and  hyperglycemic, supply insulin."
 def hypothermia_dt():
-    return "\n\n1. Get the person indoors. \n2.Dry the person off if needed. \n3.Warm the individual's torso first. \n3.Do not immerse them in warm water. \n4. Begin CPR if necessary."
+    return "\n\n1. Get the person indoors. \n2.Dry the person off if needed. \n3.Warm the individual's torso first. \n4.Do not immerse them in warm water. \n5. Begin CPR if necessary."
 def laceration_dt():
     return "\n\n1. Stop the bleeding. \n2. Clean the wound and cover with a sterile bandage."
 def overdose_dt():
-	return "\n\n1. If the individual is unconscious, place them on their side\n2. Identify the drug taken\n3. If overheating is noticed, remove unnecessary clothing to assist with cooling skin."
+	return "\n\n1. If the individual is unconscious, place them on their side.\n2. Identify the drug taken\n3. If overheating is noticed, remove unnecessary clothing to assist with cooling skin."
 def poisoning_dt():
-	return "\n\n1. For swallowed poison, remove poison in mouth\n2. If poison is on skin, remove contaminated clothing and rinse skin\n3. For poison in the eye, flush the eye with water\n4. If poison is inhaled, get the person into fresh air."
+	return "\n\n1. For swallowed poison, remove poison in mouth.\n2. If poison is on skin, remove contaminated clothing and rinse skin.\n3. For poison in the eye, flush the eye with water.\n4. If poison is inhaled, get the person into fresh air."
 def pregnancy_dt():
     return "\n\n1. Get the individual on their back\n2. Tell the individual to breathe\n3. If contractions are less than two minutes, birth is imminent\n4. Ensure that as the baby comes out, you do not pull on the baby and that you do not drop the baby."
 def seizure_dt():
@@ -68,7 +68,7 @@ def seizure_dt():
 def shooting_dt():
 	return "\n\n1. Treat both entry and exit wounds\n2. Elevate the wound\n3. Apply direct pressure and immobilize the affected limb\n4. Cover the wound with sterile gauze or clothing."
 def stabbing_dt():
-    return "\n\n1. Get the individual on their back\n2. Tell the individual to breathe\n3. If contractions are less than two minutes, birth is imminent\n4.Ensure that as the baby comes out, you do not pull on the baby and that you do not drop the baby."
+    return "\n\n1. Lie the victim down. Remove clothing around the wound.\n2. Stop the bleeding by applying pressure with sterile gauze or clothing.\n3. If the knife is in the wound, do not remove it. Clean the wound if possible."
 def stroke_dt():
 	return "\n\n1. Lay the individual on their side\n2. If the individual is not breathing, perform CPR\n3. Cover them with a blanket\n4. Do not give them anything to eat or drink."
 def unconscious_dt():
